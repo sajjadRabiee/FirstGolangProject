7d25834af80ee2bc247113ceaa9bdfbdf48a0527
Sajjad <rabiee.614@gmail.com>
2022-10-03 15:51:17 +0330

    json encode

--- controllers/mainController.go
+++ controllers/mainController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"FirstGolangProject/repositories"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -18,18 +19,20 @@ func NewHelloWorld(connection *repositories.Connection) HelloWorld {
 }
 
 func (h *helloWorld) Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.Method)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(r.Method)
 	modelsSlice := h.connection.All()
 	for _, model := range modelsSlice {
-		fmt.Fprintln(w, model)
+		encoder.Encode(model)
 	}
 }
 
 func (h *helloWorld) FindById(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.Method)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(r.Method)
 	id, _ := strconv.ParseInt(r.URL.Query().Get("id"), 0, 32)
 	model := h.connection.FindById(int(id))
-	fmt.Fprintln(w, model)
+	encoder.Encode(model)
 }
 
 func (h *helloWorld) Create(w http.ResponseWriter, r *http.Request) {
